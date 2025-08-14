02ff82371991313464146154551eb2efe6fa905c
jseo079 <jangwonseo079@gmail.com>
2025-08-14 22:06:55 +0900

    debug view in url

--- mapinfoproject/webapp/urls.py
+++ mapinfoproject/webapp/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from . import views
+from django.http import HttpResponse
+from django.core.files.storage import default_storage
+
+def storage_debug(_):
+    cls = default_storage.__class__
+    return HttpResponse(f"{cls.__module__}.{cls.__name__}")
+
 
 urlpatterns = [
     path('', views.index, name='webapp_index'),
@@ -8,5 +15,6 @@ urlpatterns = [
     path('about', views.about, name='webapp_about'),
     path('business', views.business, name='webapp_business'),
     path('contact_page', views.contact_page, name='webapp_contact_page'),
+    path("debug/storage/", storage_debug),
     # add more patterns here…
 ]
